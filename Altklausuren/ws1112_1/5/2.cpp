dd9ec7134557855e0c0ce3166b25044c94edf199
Paul Orschau <paul.orschau@icloud.com>
2018-02-13 21:54:54 +0100

    Comments

--- Altklausuren/ws1112_1/5/2.cpp
+++ Altklausuren/ws1112_1/5/2.cpp
@@ -11,22 +11,27 @@ void op(int n, int p, const float* u, const float* v, float**& m) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   
+  // Hauptprozess:
   if (rank==0) {
+    // Nur der Hauptprozess bekommt eine Matrix
     m = new float*[n];
     for (int i=0;i<n;i++) {
       m[i] = new float[p];
     }
+    // Jede Zeile füllen
     for (int i=0;i<n;i++) {
-      if (i%size==0) {
+      if (i%size==0) { // Zeile selbst ausrechnen
         for (int j=0;j<p;j++) m[i][j] = u[i]*v[j];
-      } else { 
+      } else { // Zeile empfangen
         MPI_Recv(m[i], p, MPI_FLOAT, i%size, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       }
     }
 
+  // Nebenprozesse:
   } else {
-    float* a = new float[p];
+    float* a = new float[p]; // Hilfsvektor für Zeile
     for (int i=rank;i<n;i+=size) {
+      // Nur manche Zeilen ausrechnen und an Hauptprozess senden
       for (int j=0;j<p;j++) a[j] = u[i]*v[j];
       MPI_Send(a, p, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);      
     }
@@ -38,6 +43,7 @@ int main(int argc, char* argv[]) {
   MPI_Init(&argc, &argv);
   int n = atoi(argv[1]);
   int p = atoi(argv[2]);
+  // ALLE Prozesse haben beide Vektoren
   float* u = new float[n];
   for (int i=0;i<n;i++) u[i]=cos(i);
   float* v = new float[p];
@@ -61,6 +67,8 @@ int main(int argc, char* argv[]) {
   // AUSGABE ENDE
   
   if (rank==0) {
+    // Nur der Hauptprozess darf die Matrix löschen,
+    // die Nebenprozesse haben keine
     for (int i=0;i<n;i++) delete [] m[i];
     delete [] m;
   }
